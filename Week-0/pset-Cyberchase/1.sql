be8c8e929cfad23a1910f5e3ec41c85560b2a7a0
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2024-06-17 07:26:04 +0100

    Create 1.sql

---
+++ Week-0/pset-Cyberchase/1.sql
@@ -0,0 +1,5 @@
+-- write a SQL query to list the titles of all episodes in Cyberchase’s original season, Season 1
+--results in a table with 1 column and 26 rows
+
+SELECT title FROM episodes
+WHERE season = 1;
