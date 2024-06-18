8461e5987a924710f9680b9d08404da64972f757
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2024-06-18 07:09:56 +0100

    Create 1.sql

---
+++ Week-0/pset-Views/1.sql
@@ -0,0 +1,5 @@
+-- List, side by side, the Japanese title and the English title for each print.
+-- Ensure the Japanese title is the first column, followed by the English title
+-- results in a table with 2 columns and 72 rows
+
+SELECT japanese_title, english_title FROM views;
