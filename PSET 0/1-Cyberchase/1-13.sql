62200ec73e5dd546e4e06b6dad3cc9b061f48d5c
Sarah-May Archibald <55246965+sarahmayarchibald@users.noreply.github.com>
2025-06-24 10:17:22 +0100

    Create 1-13.sql

---
+++ PSET 0/1-Cyberchase/1-13.sql
@@ -0,0 +1,5 @@
+/* write a SQL query to explore a question of your choice. This query should:
+Involve at least one condition, using WHERE with AND or OR */
+SELECT "title"
+FROM "episodes"
+WHERE "topic" LIKE '%multiplication%';
